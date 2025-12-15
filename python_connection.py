3c97da49a0cec2753ebb81057a6c52471d4f6361
zilmarij <zilmarij1229@gmail.com>
2025-12-15 19:09:00 +0530

    add: vector ops

--- python_connection.py
+++ python_connection.py
@@ -1,12 +1,28 @@
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct,VectorParams, Distance
-#qdrant_client = QdrantClient("localhost",port=6333)
-qdrant_client = QdrantClient(
-    url="https://c5748386-d8be-4bff-b52e-e88710a95438.eu-west-2-0.aws.cloud.qdrant.io:6333", 
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.dH4xAIqP1Tn8LlgrK-cwz8dDps6sDHzDwxyM8OkpZWI",
+qdrant_client = QdrantClient("localhost",port=6333)
+
+# qdrant_client = QdrantClient(
+#     url="https://c5748386-d8be-4bff-b52e-e88710a95438.eu-west-2-0.aws.cloud.qdrant.io:6333", 
+#     api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.dH4xAIqP1Tn8LlgrK-cwz8dDps6sDHzDwxyM8OkpZWI",
+# )
+
+qdrant_client.create_collection(
+    collection_name="local_collection",
+    vectors_config=VectorParams(size=4, distance=Distance.COSINE)
+)
+print(qdrant_client.get_collections())
+
+qdrant_client.upsert(
+    collection_name="local_collection",
+    wait=True,
+    points=[
+        PointStruct(id=1, vector=[0.1, 0.2, 0.3, 0.4]),
+        PointStruct(id=2, vector=[0.2, 0.3, 0.8, 0.6]),
+    ]
 )
 
-print(qdrant_client.get_collections())
+qdrant_client.query()
 '''
 qdrant_client.create_collection(
     collection_name="test_collection",
@@ -23,13 +39,13 @@ op_info = qdrant_client.upsert(
 
 print(op_info)
 '''
-search_result = qdrant_client.query_points(
-    collection_name="test_collection",
-    query=[0.2,0.1,0.9,0.7],
-    with_payload=False,
-    limit=3,
-).points
+# search_result = qdrant_client.query_points(
+#     collection_name="test_collection",
+#     query=[0.2,0.1,0.9,0.7],
+#     with_payload=False,
+#     limit=3,
+# ).points
 
-print(search_result)
+# print(search_result)
 
 #Collection with multiple Vectors:
